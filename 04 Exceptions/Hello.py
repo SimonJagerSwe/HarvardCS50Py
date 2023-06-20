f8c31a613122996a6c558a94ff2fcb005493aa67
SimonJagerSwe <123063643+SimonJagerSwe@users.noreply.github.com>
2023-06-20 12:02:57 +0200

    Add files via upload

--- 04 Exceptions/Hello.py
+++ 04 Exceptions/Hello.py
@@ -13,9 +13,99 @@ except ValueError:
 
 
 # Example 1.1
-try:
+'''try:
     x = int(input("What is x? "))
 except ValueError:
     print("x is not an integer")
 else:
+    print(f"x is {x}")'''
+
+
+# Example 2
+'''while True:
+    try:
+        x = int(input("What is x? "))
+    except ValueError:
+        print("x is not an integer")
+    else:
+        break
+
+print(f"x is {x}")'''
+
+
+# Example 2.1
+'''while True:
+    try:
+        x = int(input("What is x? "))
+        break
+    except ValueError:
+        print("x is not an integer")
+
+print(f"x is {x}")'''
+
+
+# Example 2.2
+'''def main():
+    x = get_int()
     print(f"x is {x}")
+
+def get_int():
+    while True:
+        try:
+            x = int(input("What is x? "))
+        except ValueError:
+            print("x is not an integer")
+        else:
+            break
+
+    return x
+
+main()'''
+
+
+# Example 2.3
+'''def main():
+    x = get_int()
+    print(f"x is {x}")
+
+def get_int():
+    while True:
+        try:
+            return int(input("What is x? "))
+        except ValueError:
+            print("x is not an integer")
+
+
+main()'''
+
+
+# Example 2.4
+'''def main():
+    x = get_int()
+    print(f"x is {x}")
+
+def get_int():
+    while True:
+        try:
+            return int(input("What is x? "))
+        except ValueError:
+            pass
+
+
+main()'''
+
+
+# Example 2.5
+def main():
+    x = get_int("What's x? ")
+    print(f"x is {x}")
+
+def get_int(prompt):
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError:
+            pass
+
+
+main()
